f475a221821a5254899b85f6e9cf52699eda52d3
salsh <salsh@Sally.lan>
2025-01-29 21:06:14 -0500

    adding methods

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -3,6 +3,12 @@ package demo1;
 public class DemoTest4 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		String name = "Alma";
+		String city = "Tampa";
+		
+		System.out.println(name + city);
+		
 	}
 
 }
